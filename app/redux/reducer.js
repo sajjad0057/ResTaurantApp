9695d8594e2fb867ac3c750f4771a6c9c15fb9cd
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-01-08 00:56:02 +0600

    Adding redux

---
+++ app/redux/reducer.js
@@ -0,0 +1,13 @@
+import * as actionTypes from "./actionTypes"
+const INITIAL_STATE = {
+    dishes : []
+}
+
+
+
+export const reducer =(state=INITIAL_STATE,action)=>{
+    switch(action.type){
+        default:
+            return state
+    }
+}
