3f3ade39ee5c4b2c34d127d18b589bd27811db50
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-01-09 05:00:27 +0600

    Remove FevItem From FevScreen by Pressable and Alert Component

--- app/components/FavouriteCard.jsx
+++ app/components/FavouriteCard.jsx
@@ -1,38 +1,66 @@
-import React from 'react'
-import { StyleSheet, Text, View,Image } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, Image, Pressable,Alert } from "react-native";
+import { connect } from "react-redux";
+import { removeFavourites } from "../redux/actionCreators";
 
-const FavouriteCard = props => {
-    return (
-        <View style={styles.card}>
-            <Image source={{uri:props.item.image}} style={styles.img}/>
-            <View style={styles.detail}>
-                <Text style={styles.title}>{props.item.name}</Text>
-            </View>
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeFavourites: (dishId) => dispatch(removeFavourites(dishId)),
+  };
+};
+
+const FavouriteCard = (props) => {
+    const removeFev=()=>{
+        Alert.alert(
+            "Delete Fevourite Dish ?",
+            "Are you want To Sure Delete Favourite dish"+props.item.name +" ?",
+            [
+                {
+                    text : "Cancel",
+                    onPress:()=>console.log("Cancelled"),
+                    style:"cancel"
+                },
+                {
+                    text : "OK",
+                    onPress:()=>props.removeFavourites(props.item.id)
+                }
+
+            ],
+            {cancelable:false}
+
+        )
+    }
+  return (
+    <Pressable onLongPress={()=>removeFev()}>
+      <View style={styles.card}>
+        <Image source={{ uri: props.item.image }} style={styles.img} />
+        <View style={styles.detail}>
+          <Text style={styles.title}>{props.item.name}</Text>
         </View>
-    )
-}
+      </View>
+    </Pressable>
+  );
+};
 
-export default FavouriteCard
+export default connect(null, mapDispatchToProps)(FavouriteCard);
 
 const styles = StyleSheet.create({
-    card :{
-        borderRadius:5,
-        backgroundColor:"white",
-        overflow:"hidden",
-        margin:10,
-        elevation:5,
-
-    },
-    detail :{
-        padding : 15,
-    },
-    img :{
-        width :"100%",
-        height :150,
-    },
-    title :{
-        marginBottom : 7,
-        fontSize : 20
-    }
-
-})
+  card: {
+    borderRadius: 5,
+    backgroundColor: "white",
+    overflow: "hidden",
+    margin: 10,
+    elevation: 7,
+  },
+  detail: {
+    padding: 15,
+  },
+  img: {
+    width: "100%",
+    height: 150,
+  },
+  title: {
+    marginBottom: 7,
+    fontSize: 20,
+  },
+});
